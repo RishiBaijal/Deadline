94ac921238292755cba6df03d09754b53ed4a0aa
Rishi <Rishi@Rishis-MacBook-Pro.local>
2016-01-20 15:25:51 +0530

    Added basic date picker

--- app/src/main/java/com/ip/rishi/deadline/MainActivity.java
+++ app/src/main/java/com/ip/rishi/deadline/MainActivity.java
@@ -1,16 +1,64 @@
 package com.ip.rishi.deadline;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends Activity {
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateView = (TextView) findViewById(R.id.dateView);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month + 1, day);
+
+    }
+
+    public void setDate(View view)
+    {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "Date set!", Toast.LENGTH_LONG).show();
+    }
+    protected Dialog onCreateDialog(int id)
+    {
+        if (id == 999)
+        {
+            return new DatePickerDialog(this, dateSetListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener(){
+        public void onDateSet(DatePicker dp, int year, int month, int day)
+        {
+            showDate(year, month + 1, day);
+        }
+
+    };
+    private void showDate(int year, int month, int day)
+    {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
     }
 
     @Override
